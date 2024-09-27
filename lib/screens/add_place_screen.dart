edaf799a415a3b009bb4204baee6aefbf675a72d
mgedna <etna_01@yahoo.com>
2024-09-27 11:15:01 +0300

    first commit

---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,89 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:great_places/models/place.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/image_input.dart';
+import '../providers/great_places.dart';
+import '../widgets/location_input.dart';
+import '../models/place.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = '/add-place';
+
+  @override
+  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  File _pickedImage;
+  PlaceLocation _pickedLocation;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _selectPlace(double lat, double lng) {
+    _pickedLocation = PlaceLocation(latitude: lat, longitude: lng);
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty ||
+        _pickedImage == null ||
+        _pickedLocation == null) {
+      return;
+    }
+    Provider.of<GreatPlaces>(context, listen: false)
+        .addPlace(_titleController.text, _pickedImage, _pickedLocation);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add a New Place'),
+      ),
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                      controller: _titleController,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput(_selectImage),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    LocationInput(_selectPlace),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            icon: Icon(Icons.add),
+            label: Text('Add Place'),
+            onPressed: _savePlace,
+            elevation: 0, //removes the drop shadow
+            materialTapTargetSize: MaterialTapTargetSize
+                .shrinkWrap, //a feature that ensures that you have a big space you can hit with your finger
+            color: Theme.of(context).accentColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
